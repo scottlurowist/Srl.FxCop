b51c283ccb964eac613fa57add0130dbbd8ba794
Scott Lurowist <scottlurowist@gmail.com>
2012-10-25 16:39:38 -0400

    CustomInstruction ToString now properly outputs the curly braces as if you were in the debugger.

--- Srl.FxCop.CustomRuleSdk/CustomInstruction.cs
+++ Srl.FxCop.CustomRuleSdk/CustomInstruction.cs
@@ -57,10 +57,8 @@ namespace Srl.FxCop.CustomRuleSdk
         /// </returns>
         public override string ToString()
         {
-            string valueAsString = (Value == null) ? "null": Value.ToString();
-
-            return string.Format("Opcode = {0, -10} Offset = {1, -5} Value = {2, -100}", 
-                OpCode, Convert.ToString(Offset), valueAsString);
+            return string.Format("Opcode = {0, -10} Offset = {1, -5} Value = {2, -100}",
+                OpCode, Convert.ToString(Offset), (Value == null) ? "null" : "{" + Value.ToString() + "}");
         }
     }  
 }
